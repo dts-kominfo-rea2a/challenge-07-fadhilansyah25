58b06b570c93e76d03a0b1f425f0ef2cf8d9b048
Fadil Ardiansyah <fadhilansyah25@gmail.com>
2022-06-09 21:51:55 +0700

    commit: challenge-07 REA2A done

--- promise.js
+++ promise.js
@@ -1,7 +1,20 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (hasil) => {
+  try {
+    let output = await promiseTheaterIXX().then((data) => {
+      return promiseTheaterVGC().then((data2) => {
+        return data.concat(data2);
+      });
+    });
+
+    return output.filter((data) => data.hasil === hasil).length;
+
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 module.exports = {
   promiseOutput,
